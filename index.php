c8735c57bf9f794d2fc7b1a46b825ea1bebaa10f
lewiswestcott <50587819+lewiswestcott@users.noreply.github.com>
2021-12-10 20:26:10 +0000

    updated index.php
    added reset in bar and changed in line text

--- index.php
+++ index.php
@@ -23,17 +23,12 @@
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="#">Reset</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
+                    
                 </ul>
                 <span class="navbar-text">
-                    Navbar text with an inline element
+                    lewiswestcott.co.uk
                 </span>
             </div>
         </div>
