a60cd1333a3186dee44c513ccb9f9ea28aed942c
lewiswestcott <50587819+lewiswestcott@users.noreply.github.com>
2022-01-06 11:20:31 +0000

    added function in seperate file

---
+++ _func.php
@@ -0,0 +1,45 @@
+<?php
+
+function humanFileSize($size,$unit="") 
+    {
+    if( (!$unit && $size >= 1<<30) || $unit == "GB")
+      return number_format($size/(1<<30),2)."GB";
+    if( (!$unit && $size >= 1<<20) || $unit == "MB")
+      return number_format($size/(1<<20),2)."MB";
+    if( (!$unit && $size >= 1<<10) || $unit == "KB")
+      return number_format($size/(1<<10),2)."KB";
+    return number_format($size)." bytes";
+    }
+
+    date_default_timezone_set('Europe/London');
+
+    function timeElapsedString($datetime, $full = false) {
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
+    
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+    
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+    
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
+    }
+
+?>
